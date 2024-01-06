8fa8b48a728550e4c9c781cabab62b1c3198dbdc
wling-art <wlingzhenyu@163.com>
2024-01-07 03:43:46 +0800

    ✨娶群友

---
+++ src/plugins/waifu/models.py
@@ -0,0 +1,48 @@
+from tortoise import fields
+from tortoise.models import Model
+
+# 导入插件方法
+from nonebot_plugin_tortoise_orm import add_model
+
+
+add_model("src.plugins.waifu.models")
+
+
+class WaifuProtect(Model):
+    group_id = fields.BigIntField(pk=True)
+    # 列表
+    user_id = fields.JSONField(default=[])
+    created_at = fields.DatetimeField(auto_now_add=True)
+
+    class Meta:
+        table = "waifu_protect"
+
+
+class WaifuCP(Model):
+    group_id = fields.BigIntField(pk=True)
+    # 字典
+    affect = fields.JSONField(default={})
+    created_at = fields.DatetimeField(auto_now_add=True)
+
+    class Meta:
+        table = "waifu_cp"
+
+
+class Waifu(Model):
+    group_id = fields.BigIntField(pk=True)
+    # 字典
+    waifu = fields.JSONField(default=[])
+    created_at = fields.DatetimeField(auto_now_add=True)
+
+    class Meta:
+        table = "waifu"
+
+
+class WaifuLock(Model):
+    group_id = fields.BigIntField(pk=True)
+    # 列表
+    lock = fields.JSONField(default={})
+    created_at = fields.DatetimeField(auto_now_add=True)
+
+    class Meta:
+        table = "waifu_lock"
