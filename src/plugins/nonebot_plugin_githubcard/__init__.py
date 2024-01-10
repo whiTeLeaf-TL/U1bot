b61bc43f9c610bdb65ff262fefede13cf8c698ea
wling <90316914+wling-art@users.noreply.github.com>
2024-01-11 05:59:22 +0800

    🚀 插件更新以及词云排除qq管家
    * ✨Add wordcloud_exclude_user_ids to .env file
    
    * 🚀 更新githubcard

--- src/plugins/nonebot_plugin_githubcard/__init__.py
+++ src/plugins/nonebot_plugin_githubcard/__init__.py
@@ -1,22 +1,31 @@
 from nonebot.rule import T_State
 from nonebot import get_driver
+from nonebot.log import logger
 from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent, MessageSegment
 from .config import Config
 from .data_source import get_github_reposity_information
-from nonebot.plugin import on_regex
-from nonebot.plugin import PluginMetadata
+from nonebot.plugin import on_regex, PluginMetadata
 
 import re
 
 __plugin_meta__ = PluginMetadata(
-    name="github card",
-    description="展示github仓库卡片",
-    usage="github仓库卡片\n发出 github.com/用户名/仓库名 的格式即可自动检测")
+    name="githubcard",
+    description="检测GitHub仓库链接并自动发送卡片信息（适用于Onebot V11）",
+    usage='通过正则表达式检测Github链接',
+    type='application',
+    homepage='https://github.com/ElainaFanBoy/nonebot_plugin_githubcard',
+    supported_adapters={"~onebot.v11"},
+    extra={
+        "unique_name": "githubcard",
+        "author": "Nanako <demo0929@vip.qq.com>",
+        "version": "0.2.1",
+    },
+)
+
 global_config = get_driver().config
 config = Config(**global_config.dict())
-github = on_regex(r"https?://github\.com/([^/]+/[^/]+)",
-                  priority=10,
-                  block=False)
+github = on_regex(
+    r"https?://github\.com/([^/]+/[^/]+)", priority=10, block=False)
 
 
 def match_link_parts(link):
@@ -28,6 +37,5 @@ def match_link_parts(link):
 async def github_handle(bot: Bot, event: GroupMessageEvent, state: T_State):
     url = match_link_parts(event.get_plaintext())
     imageUrl = await get_github_reposity_information(url)
-    if imageUrl == "获取信息失败":
-        raise AssertionError
+    assert (imageUrl != "获取信息失败")
     await github.send(MessageSegment.image(imageUrl))
