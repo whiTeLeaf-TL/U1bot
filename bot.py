d8e5e43f105557a9f26430a91af99e36d1a8e66b
wling <wlingzhenyu@163.com>
2024-01-20 04:17:41 +0800

    🎨

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
 import nonebot
 from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
-from sqlalchemy import StaticPool
 
-# nonebot.init(datastore_engine_options={"poolclass": StaticPool,"pool_pre_ping": True})
 nonebot.init()
 app = nonebot.get_asgi()
 driver = nonebot.get_driver()
