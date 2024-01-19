bd023b4fbbc7cd6049f03a9a7cbe97e27c259c40
wling <wlingzhenyu@163.com>
2024-01-20 04:02:22 +0800

    🐛使用数据库

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import nonebot
 from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
 from sqlalchemy import StaticPool
 
-nonebot.init(datastore_engine_options={"poolclass": StaticPool})
+nonebot.init(datastore_engine_options={"poolclass": StaticPool,"pool_pre_ping": True})
 app = nonebot.get_asgi()
 driver = nonebot.get_driver()
 driver.register_adapter(ONEBOT_V11Adapter)
