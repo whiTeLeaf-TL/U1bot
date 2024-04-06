61a5cbad2acf65428fcc069b70e7de3eb251a5e4
ajjdxns <136149181+ajjdxns@users.noreply.github.com>
2024-04-06 11:23:30 +0000

    钓鱼加了几个战利品，调大了延时时间（60）

--- src/plugins/nonebot_plugin_fishing/config.py
+++ src/plugins/nonebot_plugin_fishing/config.py
@@ -8,6 +8,12 @@ class Config(BaseModel):
 
     fishes: List[Dict] = [
         {
+            "name": "空气",
+            "frequency": 1,
+            "weight": 100,
+            "price": 0
+        },
+        {
             "name": "小鱼",
             "frequency": 2,
             "weight": 100,
@@ -20,6 +26,12 @@ class Config(BaseModel):
             "price": 1
         },
         {
+            "name": "臭袜子",
+            "frequency": 2,
+            "weight": 90,
+            "price": 2
+        },
+        {
             "name": "小杂鱼~♡",
             "frequency": 3,
             "weight": 5,
@@ -30,10 +42,16 @@ class Config(BaseModel):
             "frequency": 10,
             "weight": 1,
             "price": 50
+        },
+        {
+            "name": "破旧的钓鱼竿",
+            "frequency": 2,
+            "weight": 1,
+            "price": 30
         }
     ]
 
-    fishing_limit: int = 30
+    fishing_limit: int = 60
 
 
 config = get_plugin_config(Config)
