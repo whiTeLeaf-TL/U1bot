15231edbeec2ee46d573358c795f5637eee0c826
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2024-04-07 09:37:54 +0800

    ✨(mirror): 引入卖鱼功能 [!]
    此版本数据库存在问题，请不要合并或使用

--- src/plugins/nonebot_plugin_fishing/config.py
+++ src/plugins/nonebot_plugin_fishing/config.py
@@ -33,7 +33,7 @@ class Config(BaseModel):
         }
     ]
 
-    fishing_limit: int = 30
-
+    fishing_limit: int = 30            # 钓鱼间隔 (s)
+    fishing_coin_name: str = "次元币"  # 货币名称
 
 config = get_plugin_config(Config)
