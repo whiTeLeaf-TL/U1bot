ac134d2a449582df908af6d108ad55302ed09263
wling-art <wlingzhenyu@163.com>
2024-07-02 18:10:56 +0800

    🎨 钓鱼更新

---
+++ src/plugins/nonebot_plugin_fishing/migrations/aslfjlas132412_update_version.py
@@ -0,0 +1,28 @@
+"""
+
+"""
+
+from __future__ import annotations
+
+from collections.abc import Sequence
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = "aslfjlas132412"
+down_revision: str | Sequence[str] | None = "68463f3e5f33"
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
+
+
+def upgrade(name: str = "") -> None:
+    if name:
+        return
+    op.alter_column(
+        "nonebot_plugin_fishing_fishingrecord",
+        "coin",
+        existing_type=sa.Float(),
+        type_=sa.Float(),
+        nullable=False,
+    )
